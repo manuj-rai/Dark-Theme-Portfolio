43a4ed35c1a77566e6714fc91a31092125ae50ee
Manuj Rai <imanujrai7@gmail.com>
2024-10-21 11:22:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const words = ["Software", "Developer", "Web", "Developer"];
+const words = ["Software", "Developer", "Programmer", "Developer"];
 const typingDelay = 150; // Delay between each letter
 const wordDelay = 2000; // Delay between words
 let wordIndex = 0;
@@ -27,3 +27,16 @@ function deleteText() {
 
 // Start the typing animation
 type();
+
+function showSection(section) {
+    // Hide all sections
+    document.querySelectorAll('section').forEach(function (sec) {
+        sec.style.display = 'none';
+    });
+    
+    // Show the selected section
+    document.querySelector('.' + section).style.display = 'flex';
+}
+
+// Initialize the view by showing only the home section
+showSection('home');
